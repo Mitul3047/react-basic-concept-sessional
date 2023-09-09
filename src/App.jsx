0e946c14f5ef5e7fd07314b4b8bd55ee6e8eaa7d
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-09-09 14:49:27 +0600

    tailwind and daisy instlled

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <h1 className='text-center text-3xl'>hello</h1>
+   <button className='btn btn-primary'>Click Here</button>
+    <Greetings></Greetings>
     </>
   )
 }
+
+function Greetings(){
+  return(
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eligendi adipisci perspiciatis modi magni nam ipsum sed voluptatum repellendus perferendis. Illo tempora alias tenetur ut architecto impedit molestiae temporibus nemo!</p>
+  );
+
+}
+
+
 
 export default App
